c2a948c11b3f40ba3c538102cf491e39b512efb7
Leigh <lsteiner@locus.co>
2019-03-07 12:18:59 -0500

    api set up no permissions instated

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     # 3rd-party apps
+    'rest_framework',
     # Local
     'posts.apps.PostsConfig', 
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from .models import Post
 
 # Create your tests here.
+
+class BlogTests(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		#Create a user
+		testuser1 = User.objects.create_user(username="testuser1", password="abc123")
+		testuser1.save()
+
+		#create a blog post
+		test_post = Post.objects.create(author=testuser1, title="BlogTitle", body="body content...")
+
+	def test_blog_content(self):
+		post = Post.objects.get(id=1)
+		expected_author = f'{post.author}'
+		expected_title = f'{post.title}'
+		expected_body = f'{post.body}'
+		self.assertEqual(expected_author, 'testuser1')
+		self.assertEqual(expected_title, 'BlogTitle')
+		self.assertEqual(expected_body, 'body content...')
