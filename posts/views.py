41cae2b8d247ae6e61bf55c31a60127e6b3da326
Leigh <lsteiner@locus.co>
2019-03-07 16:20:16 -0500

    isAuthorOrReadOnly permission class functioning

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,16 @@
-from rest_framework import generics 
+from rest_framework import generics
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
-# Create your views here.
 
 class PostList(generics.ListCreateAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Post.objects.all()
-	serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
